de443e7cf0caa90910b7b943cdeae0e0047ca986
Albert Navas <albertnm76@gmail.com>
2018-12-20 10:34:22 +0100

    Exports ES6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-module.exports = {
-    restClient: require('./restClient'),
-    reducer: require('./reducer'),
-    Pagination: require('./Pagination')
-}
+export const restClient = require('./restClient')
+export const reducer = require('./reducer')
+export const Pagination = require('./Pagination')
